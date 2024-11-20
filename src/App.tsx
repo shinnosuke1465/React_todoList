8a5fe0deaf0c64899debef0b69a5e88da03525d3
鈴木新之介 <shin.suzuki0605@gmail.com>
2024-11-20 15:32:42 +0900

    todo実装

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,54 @@ function App() {
     checked: boolean;
   };
 
+  //inputの値を取得更新
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
     setInputText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  //formが送信された時にtodoリストに情報を入れる
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); //送信ボタンを押した時にページ全体がリロードされないようにする
 
     //新しいtodo作成
+    const newTodo: Todo = {
+      inputValue: inputText,
+      id: todos.length,
+      checked: false,
+    }
+
+    setTodos([newTodo, ...todos]);
+    setInputText(""); //投稿した後はinputを空にする
+  };
+
+  //表示したタスクの編集機能(handleEdit関数)
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  //チェックボックス実装
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  //削除ボタン実装
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id); //idがあっていないものを残す
+    setTodos(newTodos);
   };
 
   return (
@@ -31,6 +69,16 @@ function App() {
         <input type="text" onChange={(e) => handleChange(e)} aria-label="タスク" className='inputText'/>
         <input type="submit" value="作成" className="submitButton" />
       </form>
+      {/* todoリストに入れたタスクをmap関数で一つ一つ取り出して表示する */}
+      <ul className='todoList'>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="text" onChange={(e) => handleEdit(todo.id, e.target.value)} className='inputText' aria-label="タスク" value={todo.inputValue} disabled={todo.checked}/>
+            <input type="checkbox" onChange={(e) => handleChecked(todo.id, todo.checked)} aria-label="チェックボックス"/>
+            <button onClick={() => handleDelete(todo.id)}>消</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
