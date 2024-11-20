ee4c2e03093a6c7d6181a108c41130e7bebc1dc4
鈴木新之介 <shin.suzuki0605@gmail.com>
2024-11-20 14:13:47 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //送信ボタンを押した時にページ全体がリロードされないようにする
+
+    //新しいtodo作成
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Todoリスト</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" onChange={(e) => handleChange(e)} aria-label="タスク" className='inputText'/>
+        <input type="submit" value="作成" className="submitButton" />
+      </form>
     </div>
   );
 }
